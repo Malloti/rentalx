6215aef3634d0c3edb849e8c2902b1a68fd4cff1
Felipe Freitas <felipe96.ff@gmail.com>
2022-08-01 21:12:26 -0300

    add funcionalidade para salvar usuários

---
+++ src/modules/accounts/repositories/implementations/UserRepository.ts
@@ -0,0 +1,22 @@
+import { Repository } from 'typeorm';
+
+import AppDataSource from '../../../../database/data-source';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(User);
+  }
+
+  async create(data: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create(data);
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
