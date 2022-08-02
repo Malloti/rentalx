990adda10c4149fb45dad27750ca81a5708fee3d
Felipe Freitas <felipe96.ff@gmail.com>
2022-08-01 22:21:14 -0300

    fix eslint problem

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
@@ -20,18 +20,16 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      message: `Internal server error ${err.message}`,
+app.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
     });
-  },
-);
+  }
+
+  return response.status(500).json({
+    message: `Internal server error ${err.message}`,
+  });
+});
 
 app.listen(3333, () => console.log('Server is running at port 3333'));
